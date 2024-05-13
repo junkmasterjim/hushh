4f66e5721948d3bf230503681da323cc68b72e57
Noah Pittman <noahpittman00@gmail.com>
2024-05-13 14:20:48 -0400

    init. styling & menu bar application

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,109 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Slider } from "@/components/ui/slider";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { PauseIcon, PlayIcon } from "lucide-react";
+import { useState } from "react";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const NOISE_TYPES = ["white", "pink", "rain", "fire"];
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const Home = () => {
+	const [noiseType, setNoiseType] = useState<string>("white");
+	const [volume, setVolume] = useState<number>(75);
+	const [playing, setPlaying] = useState<boolean>(false);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	return (
+		<main className="space-y-4">
+			<header className="flex items-center justify-between">
+				<span>
+					<h1 className="text-lg font-medium">hushh</h1>
+					<h2 className="text-xs">White noise generator.</h2>
+				</span>
+				<PlayPauseButton />
+			</header>
+			<Separator />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+			{/* select noise type (white noise, pink noise, light rain, heavy rain, fire crackle) with radio buttons */}
+			<div className="flex flex-col gap-2">
+				<label className="block text-sm font-medium">Noise type</label>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+				<RadioGroup value={noiseType} onValueChange={(e) => setNoiseType(e)}>
+					{NOISE_TYPES.map((type) => (
+						<li
+							className="flex items-center gap-2 cursor-pointer"
+							key={type}
+							onClick={() => setNoiseType(type)}
+						>
+							<Label className="text-xs lowercase cursor-pointer">{type}</Label>
+							<RadioGroupItem value={type} />
+						</li>
+					))}
+				</RadioGroup>
+			</div>
+
+			<Separator />
+
+			{/* volume slider */}
+			<div className="flex flex-col gap-2">
+				<label className="block text-sm font-medium text-neutral-900">
+					Volume {volume}%
+				</label>
+				<Slider
+					typeof="range"
+					min={0}
+					max={100}
+					value={[volume]}
+					onValueChange={(value) => setVolume(value[0])}
+					step={5}
+				/>
+			</div>
+		</main>
+	);
+};
+
+export default Home;
+
+const Separator = () => {
+	return <div className="h-px bg-neutral-300" />;
+};
+
+const PlayPauseButton = () => {
+	const [playing, setPlaying] = useState<boolean>(false);
+
+	return (
+		<Tooltip delayDuration={500}>
+			<TooltipTrigger asChild>
+				<button
+					id="play-pause-button"
+					className="p-1 rounded-full bg-neutral-300 inline-flex items-center justify-center group hover:opacity-75 transition-opacity"
+					onClick={() => setPlaying(!playing)}
+					onMouseDown={() =>
+						document
+							.getElementById("play-pause-button")
+							?.classList.add("scale-95")
+					}
+					onMouseUp={() =>
+						document
+							.getElementById("play-pause-button")
+							?.classList.remove("scale-95")
+					}
+				>
+					{playing ? (
+						<PauseIcon className="size-3" fill="currentColor" />
+					) : (
+						<PlayIcon className="size-3" fill="currentColor" />
+					)}
+				</button>
+			</TooltipTrigger>
+			<TooltipContent side="left" align="center">
+				<p>{playing ? "Pause" : "Play"}</p>
+			</TooltipContent>
+		</Tooltip>
+	);
+};
